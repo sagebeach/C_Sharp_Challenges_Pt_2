2b5fd599e28363da6894f726215894da56a0af14
Sage Beach <32965757+sagebeach@users.noreply.github.com>
2018-03-22 10:48:15 -0700

    First MVC application
    A small Master-Controller type application to further my understanding of how MVC integrates between views and controllers

---
+++ MVC_ComicBookChallenge/FirstChallenge/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var comicBooks = ComicBookModel.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var comicBooks = ComicBookModel.GetComicBooks();
+            var comic = comicBooks.FirstOrDefault(p => p.ComicBookId == id);
+            return View(comic);
+        }
+    }
+}
